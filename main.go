888b79b665d69578ca7d2aa170951e4ddf369645
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2022-11-17 00:10:42 +0100

    Add doc and update routes.

--- main.go
+++ main.go
@@ -1,23 +1,75 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Try /recipes or /recipesByItem/egg for all recipes with egg")
-	})
 
-	r.GET("/recipes", func(c *gin.Context) {
-		c.JSON(200, recipes)
-	})
+	// Management routes
+	// Only for >= moderators only
+	managementRoutes := r.Group("/manag")
+	{
+		// Recipes
+		recipeRoutes := managementRoutes.Group("/recipe")
 
-	r.GET("/recipesByItem/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.JSON(200, FindRecipesByItems(ItemNameToItem([]string{name})))
-	})
+		// Get list of all recipes
+		recipeRoutes.GET("/", func(c *gin.Context) {
+			c.JSON(200, recipes)
+		})
+
+		// Add a recipe
+		recipeRoutes.POST("/", func(c *gin.Context) {
+			var newRecipe Recipe
+			c.BindJSON(&newRecipe)
+			AddRecipe(newRecipe)
+			c.JSON(200, recipes)
+		})
+
+		// Update a recipe
+		recipeRoutes.PUT("/", func(c *gin.Context) {
+			// not implemented yet
+			c.AbortWithStatus(501)
+		})
+
+		// Users
+		userRoutes := r.Group("/user")
+
+		// Add an user
+		userRoutes.POST("/", func(c *gin.Context) {
+			var newUser User
+			c.BindJSON(&newUser)
+			AddUser(newUser.Username, newUser.Password)
+			c.String(200, "Added user")
+		})
+	}
+
+	// Recipes routes
+	recipeRoutes := r.Group("/recipe")
+	{
+		recipeRoutes.GET("/byItem/:name", func(c *gin.Context) {
+			name := c.Param("name")
+			c.JSON(200, FindRecipesByItems(ItemNameToItem([]string{name})))
+		})
+	}
+
+	// User routes
+	userRoutes := r.Group("/user")
+	{
+		userRoutes.GET("/", func(c *gin.Context) {
+			c.String(200, "Try /user/1")
+		})
+
+		userRoutes.GET("/:id", func(c *gin.Context) {
+			id, err := strconv.Atoi(c.Param("id"))
+			if err == nil {
+				c.JSON(200, FindUserById(id))
+			}
+		})
+	}
 
 	err := r.Run()
 	if err != nil {
