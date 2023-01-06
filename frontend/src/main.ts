3c58fc0f222c06fdb761cfa736c76385bdf0da64
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-01-06 16:33:39 +0100

    Add PWA support

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,9 @@ import router from './router';
 
 import { IonicVue } from '@ionic/vue';
 
+/* Add service worker */
+import './registerServiceWorker';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
