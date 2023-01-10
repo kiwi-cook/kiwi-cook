8e197c55e6215abc8ffb8d82328d8bcdeed3375c
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-01-10 22:40:31 +0100

    Add function which translates an enum to a route

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -5,8 +5,30 @@ export const API_URL = 'http://localhost:8081/api'
 export const API_VERSION = '/v1'
 
 // basic API routes
-export const API_ROUTES = {
+const API_ROUTES = {
     RECIPES: '/recipe',
+    ITEMS: '/item',
     DISCOUNTS: '/discount',
     MARKETS: '/market',
+}
+
+export enum API_ROUTE {
+    RECIPES,
+    ITEMS,
+    DISCOUNTS,
+    MARKETS
+}
+
+// get API route by type
+export const getApiRoute = (route: API_ROUTE): string => {
+    const basis = API_URL + API_VERSION;
+    switch (route) {
+        case API_ROUTE.ITEMS:
+            return basis + API_ROUTES.ITEMS;
+        case API_ROUTE.DISCOUNTS:
+            return basis + API_ROUTES.DISCOUNTS;
+        case API_ROUTE.MARKETS:
+            return basis + API_ROUTES.MARKETS;
+    }
+    return basis + API_ROUTES.RECIPES;
 }
