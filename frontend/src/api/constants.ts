38a4ff08a50569edd4073fc46551d1efaed2edc9
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-01-10 22:23:53 +0100

    Add basic API constants

---
+++ frontend/src/api/constants.ts
@@ -0,0 +1,12 @@
+// URLs for the API
+
+// API constants
+export const API_URL = 'http://localhost:8081/api'
+export const API_VERSION = '/v1'
+
+// basic API routes
+export const API_ROUTES = {
+    RECIPES: '/recipe',
+    DISCOUNTS: '/discount',
+    MARKETS: '/market',
+}
