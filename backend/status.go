db40b5553b94f837246e26c808e081bed81f46fe
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-02-05 21:10:25 +0100

    [backend] Add status related methods that handle errors and successes

---
+++ backend/status.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
+)
+
+func Success(context *gin.Context, message string) {
+	context.JSON(http.StatusOK, gin.H{"success": message})
+}
+
+func SuccessJSON(context *gin.Context, json any) {
+	context.JSON(http.StatusOK, json)
+}
+
+func NotFoundError(context *gin.Context, itemName string) {
+	context.JSON(http.StatusNotFound, gin.H{"error": itemName + " not found."})
+}
+
+func BadRequestError(context *gin.Context, message string) {
+	context.JSON(http.StatusBadRequest, gin.H{"error": message})
+}
+
+func ServerError(context *gin.Context, funny bool) {
+	var message string
+	if funny {
+		message = funnyErrorMessage()
+	} else {
+		message = "Internal Server ErrorMessage"
+	}
+	ErrorMessage(context, message)
+}
+
+func ErrorMessage(context *gin.Context, message string) {
+	context.JSON(http.StatusInternalServerError, gin.H{"error": message})
+}
+
+func funnyErrorMessage() string {
+	messages := []string{
+		"WTH are you trying to do?",
+		"Oh Lord, we're being hacked!",
+		"Nope.",
+		"Stop it!",
+		"We lost him",
+		"Hello, this is Microsoft Tech support. How can I help you?",
+		"Beep boop. This is an error.",
+		"Oh My God! You killed Kenny!",
+	}
+
+	randomIndex := rand.Intn(len(messages))
+	return messages[randomIndex]
+}
