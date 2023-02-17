0ebb497cc82dd59b2720940c55dd045bef37c17d
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-02-17 13:59:57 +0100

    [backend] Add more funny error messages

--- backend/status.go
+++ backend/status.go
@@ -38,17 +38,35 @@ func (context *TasteBuddyContext) ErrorMessage(message string) {
 }
 
 func funnyErrorMessage() string {
-	messages := []string{
-		"WTH are you trying to do?",
-		"Oh Lord, we're being hacked!",
-		"Nope.",
-		"Stop it!",
-		"We lost him",
+	funnyMessages := []string{
+		"Looks like you're trying to break the internet again. Please stop, it's already had a rough year.",
+		"Oh Lord, we're being hacked! 😱 Call the FBI!",
 		"Hello, this is Microsoft Tech support. How can I help you?",
-		"Beep boop. This is an error.",
+		"Beep boop. This is an error. Boop beep! 🤖",
 		"Oh My God! You killed Kenny!",
+		"Something happened. We're not sure what, but something happened.",
+		"Looks like you're barking up the wrong API! Try another endpoint.",
+		"Oh no! The server is feeling a bit overwhelmed. Give it a minute to catch its breath.",
+		"Whoops! It seems our hamsters powering the server just took a coffee break. Try again in a few minutes.",
+		"ERROR 404: Wisdom not found. Please try a different endpoint.",
+		"Looks like the squirrels running our database went on strike. Try again later.",
+		"We're sorry, but your request has been lost in space. Literally. Please try again.",
+		"Error: Cannot divide by zero. We're pretty sure even the universe can't handle that.",
+		"Uh-oh! We couldn't find what you were looking for. Maybe try using a telescope?",
+		"Error: Insufficient caffeine levels detected. Our developers are working on a fix.",
+		"Oops! Our servers seem to be experiencing some technical difficulties. But don't worry, we've sent in the clowns to fix it.",
+		"Error: Your request was a little too wild for our servers to handle. Please try again with a tamer request.",
+		"We apologize for the inconvenience, but our server is currently on vacation. Try again when it gets back.",
+		"Oops! Looks like our developers forgot to pay the internet bill. Try again later when we have Wi-Fi again.",
+		"Error: Your request made our servers go bananas. Maybe try a less fruity approach?",
+		"Uh-oh, looks like we spilled coffee on our servers again. Please stand by while we clean up the mess.",
+		"We're sorry, but the data you're looking for seems to have gone missing. Maybe it eloped with a unicorn?",
+		"Error: Our hamsters fell off their wheel and the server crashed. We're currently coaxing them back on track.",
+		"Looks like you're trying to break the internet again. Please stop, it's already had a rough year.",
+		"Error: We couldn't find the page you were looking for. Maybe try turning it off and on again?",
+		"We apologize, but our servers are currently in a time-out. They need to think about what they've done.",
 	}
 
-	randomIndex := rand.Intn(len(messages))
-	return messages[randomIndex]
+	randomIndex := rand.Intn(len(funnyMessages))
+	return funnyMessages[randomIndex]
 }
