4ebbe45485820f8cfb2ae171909d6feeacd397a2
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-02-20 23:16:25 +0100

    [frontend] Update recipe editor

--- frontend/src/components/RecipeEditor.vue
+++ frontend/src/components/RecipeEditor.vue
@@ -1,8 +1,5 @@
 <template>
-    <!-- Recipe information -->
     <ion-card>
-        <ion-img :alt="`Image of ${mutableRecipe.name}`"
-            :src="`https://source.unsplash.com/random/300x200?${mutableRecipe.name}`" />
         <ion-card-header>
             <ion-text color="tertiary">
                 ID {{ mutableRecipe._id }}
@@ -38,53 +35,57 @@
     <template v-for="(step, stepIndex) in steps" :key="stepIndex">
         <ion-card>
             <ion-card-header>
-                <ion-card-title color="primary">Schritt {{ stepIndex + 1}}</ion-card-title>
+                <ion-card-title color="primary">Schritt {{ stepIndex + 1 }}</ion-card-title>
             </ion-card-header>
 
+            <ion-item>
+                <ion-label color="light">Description</ion-label>
+                <ion-input color="light" v-model="step.description" />
+            </ion-item>
+
             <ion-card-content>
-                <div>
-                    <!-- Items -->
-                    <template v-for="(item, itemIndex) in step.items" :key="stepIndex + ' - ' + itemIndex">
-                        <ion-item>
-                            <ion-thumbnail slot="start">
-                                <!-- Add Item image -->
-                                <img :alt="`Image of ${item.itemID}`"
-                                    :src="`https://source.unsplash.com/random/50x50?${item.itemID}`" />
-                            </ion-thumbnail>
-                            <!-- ID/name -->
-                            <ion-label color="light">Item</ion-label>
-                            <ion-input color="light" v-model="item.itemID" />
+                <!-- Items -->
+                <template v-for="(stepItem, itemIndex) in step.items" :key="stepIndex + ' - ' + itemIndex">
+                    <ion-card>
+                        <ion-card-header>
+                            <ion-card-title color="primary">
+                                <ion-input v-model="stepItem.item.name" color="primary" />
+                            </ion-card-title>
+                        </ion-card-header>
 
-                            <!-- amount -->
-                            <ion-label color="light">Amount</ion-label>
-                            <ion-input color="light" v-model.number="item.amount" />
+                        <ion-card-content>
+                            <ion-item>
+                                <ion-label color="light">URL</ion-label>
+                                <ion-input color="light" v-model="stepItem.item.imgUrl" />
+                            </ion-item>
 
-                            <!-- amount -->
-                            <ion-select color="light" placeholder="Unit">
-                                <ion-select-option value="ml">ml</ion-select-option>
-                                <ion-select-option value="g">g</ion-select-option>
-                            </ion-select>
-                        </ion-item>
-                    </template>
-                    <ion-button fill="clear" @click="addItem(stepIndex)">Add item</ion-button>
-                </div>
-                <ion-item>
-                    <ion-label color="light">Description</ion-label>
-                    <ion-input color="light" v-model="step.description" />
-                </ion-item>
+                            <ion-item>
+                                <ion-label color="light">Amount</ion-label>
+                                <ion-input color="light" v-model.number="stepItem.amount" />
+                            </ion-item>
+
+                            <ion-item>
+                                <ion-select color="light" placeholder="Unit">
+                                    <ion-select-option value="ml">ml</ion-select-option>
+                                    <ion-select-option value="g">g</ion-select-option>
+                                </ion-select>
+                            </ion-item>
+                        </ion-card-content>
+                    </ion-card>
+                </template>
+                <ion-button fill="clear" @click="addItem(stepIndex)">Add item</ion-button>
                 <ion-button fill="clear" @click="addStep()">Add step</ion-button>
             </ion-card-content>
         </ion-card>
-
+        <ion-button @click="saveRecipe()">Save recipe</ion-button>
     </template>
-    <ion-button @click="saveRecipe()">Save recipe</ion-button>
 </template>
 
 <script lang="ts">
 import { getFromAPI } from '@/api';
 import { API_ROUTE } from '@/api/constants';
 import { Recipe } from '@/api/types';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonThumbnail, IonInput, IonItem, IonLabel, IonCardSubtitle, IonImg, IonSelect, IonSelectOption, IonText } from '@ionic/vue';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonInput, IonItem, IonLabel, IonCardSubtitle, IonSelect, IonSelectOption, IonText } from '@ionic/vue';
 import { computed, defineComponent, PropType, ref, toRefs } from 'vue';
 
 export default defineComponent({
@@ -96,14 +97,12 @@ export default defineComponent({
         },
     },
     components: {
-        IonImg,
         IonText,
         IonCard,
         IonCardHeader,
         IonCardTitle,
         IonCardSubtitle,
         IonCardContent,
-        IonThumbnail,
         IonItem,
         IonLabel,
         IonInput,
@@ -126,9 +125,14 @@ export default defineComponent({
 
         const addItem = (stepIndex: number) => {
             steps.value[stepIndex].items.push({
-                itemID: '',
                 amount: 0,
                 unit: '',
+                item: {
+                    _id: '',
+                    name: '',
+                    type: '',
+                    imgUrl: '',
+                },
             });
         };
 
