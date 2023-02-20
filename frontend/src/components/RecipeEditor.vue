0d09d0fc806b00397c399ba92c3c1b1fe9926ecf
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-02-21 00:19:50 +0100

    [frontend] Streamline and simplify design of recipe editor

--- frontend/src/components/RecipeEditor.vue
+++ frontend/src/components/RecipeEditor.vue
@@ -1,29 +1,27 @@
 <template>
-    <ion-card>
+    <ion-card class="recipe-editor">
         <ion-card-header>
-            <ion-text color="tertiary">
-                ID {{ mutableRecipe._id }}
-            </ion-text>
-            <ion-card-title color="primary">
-                <ion-item>
-                    <ion-label color="primary">Recipe name</ion-label>
-                    <ion-input color="light" v-model="mutableRecipe.name" />
-                </ion-item>
-            </ion-card-title>
-            <ion-card-subtitle color="light">
-                <ion-item>
-                    <ion-label color="primary">Author</ion-label>
-                    <ion-input color="light" v-model="mutableRecipe.author" />
-                </ion-item>
-
-                <ion-item>
-                    <ion-label color="primary">Cooking time</ion-label>
-                    <ion-input color="light" v-model.number="mutableRecipe.cookingTime" />
-                </ion-item>
-            </ion-card-subtitle>
+            <ion-card-header>
+                <ion-card-title color="primary">
+                    <ion-input v-model="mutableRecipe.name" color="primary" />
+                </ion-card-title>
+                <ion-text color="tertiary">
+                    ID {{ mutableRecipe._id }}
+                </ion-text>
+            </ion-card-header>
         </ion-card-header>
 
         <ion-card-content color="light">
+            <ion-item>
+                <ion-label color="primary">Author</ion-label>
+                <ion-input color="light" v-model="mutableRecipe.author" />
+            </ion-item>
+
+            <ion-item>
+                <ion-label color="primary">Cooking time</ion-label>
+                <ion-input color="light" v-model.number="mutableRecipe.cookingTime" />
+            </ion-item>
+
             <ion-item>
                 <ion-label color="primary">Description</ion-label>
                 <ion-input color="light" v-model="mutableRecipe.description" />
@@ -32,10 +30,12 @@
     </ion-card>
 
     <!-- Steps -->
+    <ion-button fill="clear" @click="addStep(-1)">Add step</ion-button>
     <template v-for="(step, stepIndex) in steps" :key="stepIndex">
-        <ion-card>
+
+        <ion-card class="step-editor">
             <ion-card-header>
-                <ion-card-title color="primary">Schritt {{ stepIndex + 1 }}</ion-card-title>
+                <ion-card-title color="primary">Step {{ stepIndex + 1 }}</ion-card-title>
             </ion-card-header>
 
             <ion-item>
@@ -43,24 +43,31 @@
                 <ion-input color="light" v-model="step.description" />
             </ion-item>
 
-            <ion-card-content>
+            <ion-card-content class="items-editor">
                 <!-- Items -->
                 <template v-for="(stepItem, itemIndex) in step.items" :key="stepIndex + ' - ' + itemIndex">
-                    <ion-card>
+                    <ion-card class="item-editor">
                         <ion-card-header>
+                            <ion-thumbnail v-if="stepItem.item.imgUrl">
+                                <img :src="stepItem.item.imgUrl" />
+                            </ion-thumbnail>
                             <ion-card-title color="primary">
-                                <ion-input v-model="stepItem.item.name" color="primary" />
+                                <ion-input v-model="stepItem.item.name" placeholder="Itemname" color="primary" />
                             </ion-card-title>
+                            <ion-text color="tertiary">
+                                ID {{ stepItem.item._id }}
+                            </ion-text>
                         </ion-card-header>
 
                         <ion-card-content>
                             <ion-item>
-                                <ion-label color="light">URL</ion-label>
-                                <ion-input color="light" v-model="stepItem.item.imgUrl" />
+                                <ion-label color="light" position="stacked">URL</ion-label>
+                                <ion-input color="light" :placeholder="`Image URL for ${stepItem.item.name}`"
+                                    v-model="stepItem.item.imgUrl" />
                             </ion-item>
 
                             <ion-item>
-                                <ion-label color="light">Amount</ion-label>
+                                <ion-label color="light" position="stacked">Amount</ion-label>
                                 <ion-input color="light" v-model.number="stepItem.amount" />
                             </ion-item>
 
@@ -73,19 +80,19 @@
                         </ion-card-content>
                     </ion-card>
                 </template>
-                <ion-button fill="clear" @click="addItem(stepIndex)">Add item</ion-button>
-                <ion-button fill="clear" @click="addStep()">Add step</ion-button>
             </ion-card-content>
+            <ion-button fill="clear" @click="addItem(stepIndex)">Add item</ion-button>
         </ion-card>
-        <ion-button @click="saveRecipe()">Save recipe</ion-button>
+        <ion-button fill="clear" @click="addStep(stepIndex)">Add step</ion-button>
     </template>
+    <ion-button @click="saveRecipe()">Save recipe</ion-button>
 </template>
 
 <script lang="ts">
 import { getFromAPI } from '@/api';
 import { API_ROUTE } from '@/api/constants';
 import { Recipe } from '@/api/types';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonInput, IonItem, IonLabel, IonCardSubtitle, IonSelect, IonSelectOption, IonText } from '@ionic/vue';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonInput, IonItem, IonLabel, IonSelect, IonSelectOption, IonText } from '@ionic/vue';
 import { computed, defineComponent, PropType, ref, toRefs } from 'vue';
 
 export default defineComponent({
@@ -101,7 +108,6 @@ export default defineComponent({
         IonCard,
         IonCardHeader,
         IonCardTitle,
-        IonCardSubtitle,
         IonCardContent,
         IonItem,
         IonLabel,
@@ -116,8 +122,8 @@ export default defineComponent({
         const mutableRecipe = ref<Recipe>(recipe.value)
         const steps = computed(() => mutableRecipe.value.steps);
 
-        const addStep = () => {
-            steps.value.push({
+        const addStep = (stepIndex: number) => {
+            steps.value.splice(stepIndex + 1, 0, {
                 description: '',
                 items: [],
             });
@@ -148,3 +154,30 @@ export default defineComponent({
     },
 })
 </script>
+
+<style>
+.step-editor,
+.item-editor {
+    /* nice shadow with light background and round corners */
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2), 0 0 10px 0 rgba(0, 0, 0, 0.19);
+}
+
+.recipe-editor {
+    margin: 10px;
+}
+
+.step-editor {
+    margin: 10px;
+}
+
+.items-editor {
+    margin: 10px;
+    max-width: fit-content;
+    display: flex;
+}
+
+.item-editor {
+    margin: 10px;
+    max-width: fit-content;
+}
+</style>
