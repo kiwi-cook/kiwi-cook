3bc6bd0aa3139c1da11f10fdf89a08e87ffa95ac
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-02-21 15:12:15 +0100

    Revert change in recipe.go

--- backend/recipe.go
+++ backend/recipe.go
@@ -10,7 +10,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Recipe struct {
