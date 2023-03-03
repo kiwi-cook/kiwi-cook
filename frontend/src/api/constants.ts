a418598141eb3013574db74411068dc532f844db
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-03-03 17:24:54 +0100

    [frontend] Add support to delete recipes

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -7,6 +7,7 @@ export const API_VERSION = '/v1'
 export enum API_ROUTE {
     GET_RECIPES,
     ADD_RECIPE,
+    DELETE_RECIPE,
     GET_ITEMS,
     GET_DISCOUNTS,
     GET_MARKETS,
@@ -15,6 +16,7 @@ export enum API_ROUTE {
 export const API_ROUTES: { [key in API_ROUTE]: any } = {
     [API_ROUTE.GET_RECIPES]: { url: '/recipe/', method: 'GET' },
     [API_ROUTE.ADD_RECIPE]: { url: '/recipe/', method: 'POST' },
+    [API_ROUTE.DELETE_RECIPE]: { url: '/recipe/RECIPE_ID', method: 'DELETE' },
     [API_ROUTE.GET_ITEMS]: { url: '/item/', method: 'GET' },
     [API_ROUTE.GET_DISCOUNTS]: { url: '/discount/CITY', method: 'GET' },
     [API_ROUTE.GET_MARKETS]: { url: '/discount/markets/CITY', method: 'GET' }
