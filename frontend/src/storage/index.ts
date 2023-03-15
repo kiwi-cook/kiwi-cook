4c7a765d89935ace751773141e5dcc2c5890d32c
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-03-16 00:01:41 +0100

    [frontend] Improve API integration in store

--- frontend/src/storage/index.ts
+++ frontend/src/storage/index.ts
@@ -45,54 +45,125 @@ export function createVueStore() {
             recipesByItemId: {}
         },
         mutations: {
-            setRecipes(state, recipes) {
-                state.recipes = recipes
+            setRecipes(state, recipes: Recipe[]) {
+                state.recipes = recipes.sort((a: Recipe, b: Recipe) => a.name.localeCompare(b.name))
             },
-            setItems(state, items) {
-                state.items = items
+            updateRecipe(state, recipe: Recipe) {
+                // try to find the recipe in the list
+                const recipeIndex = state.recipes.findIndex((r: Recipe) => (r._id === undefined && r._tmpId === recipe._tmpId) || (r._tmpId === undefined && r._id === recipe._id))
+                console.log('recipeIndex', recipeIndex);
+                console.log(state.recipes)
+                if (recipeIndex !== -1) {
+                    state.recipes[recipeIndex] = recipe
+                } else {
+                    state.recipes.push(recipe)
+                }
+                state.recipes = state.recipes.sort((a: Recipe, b: Recipe) => a.name.localeCompare(b.name))
+            },
+            removeRecipe(state, recipe: Recipe) {
+                state.recipes = state.recipes.filter((r: Recipe) => r._id !== recipe._id)
+            },
+            setItems(state, items: Item[]) {
+                state.items = items.sort((a: Item, b: Item) => a.name.localeCompare(b.name))
+            },
+            updateItem(state, item: Item) {
+                // try to find the item in the list
+                const itemIndex = state.items.findIndex((i: Item) => i._tmpId === item._tmpId || i._id === item._id)
+                if (itemIndex !== -1) {
+                    state.items[itemIndex] = item
+                } else {
+                    state.items.push(item)
+                }
+                state.items = state.items.sort((a: Item, b: Item) => a.name.localeCompare(b.name))
+            },
+            removeItem(state, item: Item) {
+                state.items = state.items.filter((i: Item) => i._id !== item._id)
             },
             setDiscounts(state, payload: { city: string, discounts: Discount[] }) {
                 const { city, discounts } = payload
                 state.discounts[city] = discounts
             },
-            mapRecipeIdsToItemIds(state, recipes) {
-                state.recipesByItemId = recipes
-                    .reduce((recipesIdByItemId: { [itemId: string]: string[] }, recipe: Recipe) => {
-                        getItemsFromRecipe(recipe)
-                            .forEach((item: Item) => {
-                                if (typeof item._id !== 'undefined' && typeof recipe._id !== 'undefined') {
-                                    if (typeof recipesIdByItemId[item._id] === 'undefined') {
-                                        recipesIdByItemId[item._id] = []
-                                    }
-                                    recipesIdByItemId[item._id].push(recipe._id)
-                                }
-                            })
+            mapRecipeIdsToItemIds(state, recipes: Recipe[]) {
+                const recipesByItemId: { [key: string]: string[] } = {}
 
-                        return recipesIdByItemId
-                    }, {})
-            }
+                recipes.forEach((recipe: Recipe) => {
+                    const items = recipe.getItems()
+                    items.forEach((item: Item) => {
+                        if (!(item.getId() in recipesByItemId)) {
+                            recipesByItemId[item.getId()] = []
+                        }
+                        recipesByItemId[item.getId()].push(recipe.getId())
+                    })
+                })
+
+                state.recipesByItemId = recipesByItemId
+            } 
         },
         actions: {
             async fetchRecipes({ commit }) {
-                getFromAPI(API_ROUTE.GET_RECIPES, {
-                    callback: (json: Recipe[]) => {
-                        commit('setRecipes', json)
-                    }
+                getFromAPI<Recipe>(API_ROUTE.GET_RECIPES)
+                    .then((recipes: Recipe[]) => {
+                        // map the recipes JSON to Recipe objects
+                        // this is because the JSON is not a valid Recipe object
+                        // and we need to use the Recipe class methods
+                        commit('setRecipes', recipes.map((recipe: Recipe) => Recipe.fromJSON(recipe)))
+                    });
+            },
+            async saveRecipeById({ commit, getters }, recipeId: string) {
+                const recipe: Recipe = getters.getRecipeById[recipeId]
+                if (typeof recipe === 'undefined') {
+                    console.error('Recipe not found: ', recipeId)
+                    return
+                }
+                this.dispatch('saveRecipe', recipe)
+            },
+            async saveRecipe({ commit }, recipe: Recipe) {
+                commit('updateRecipe', recipe)
+                getFromAPI(API_ROUTE.ADD_RECIPE, {
+                    body: recipe
+                }).then(() => {
+                    this.dispatch('fetchRecipes')
                 });
+            },
+            async deleteRecipe({ commit }, recipe: Recipe) {
+                commit('removeRecipe', recipe)
+                if (typeof recipe._id !== 'undefined') {
+                    getFromAPI(API_ROUTE.DELETE_RECIPE, {
+                        formatObject: { RECIPE_ID: recipe._id ?? '' }
+                    })
+                }
             },
             async fetchItems({ commit }) {
-                getFromAPI(API_ROUTE.GET_ITEMS, {
-                    callback: (json: Item[]) => {
-                        commit('setItems', json)
-                    }
+                getFromAPI<Item>(API_ROUTE.GET_ITEMS)
+                    .then((items: Item[]) => {
+                        // map the items JSON to Item objects
+                        // this is because the JSON is not a valid Item object
+                        // and we need to use the Item class methods
+                        commit('setItems', items.map((item: Item) => Item.fromJSON(item)))
+                    });
+            },
+            async saveItem({ commit }, item: Item) {
+                commit('updateItem', item)
+                getFromAPI(API_ROUTE.ADD_ITEM, {
+                    body: item
+                }).then(() => {
+                    this.dispatch('fetchItems')
                 });
             },
+            async deleteItem({ commit }, item: Item) {
+                commit('removeItem', item)
+                if (typeof item._id !== 'undefined') {
+                    getFromAPI(API_ROUTE.DELETE_ITEM, {
+                        formatObject: { ITEM_ID: item._id ?? '' }
+                    })
+                }
+            },
             async fetchDiscounts({ commit }, city: string) {
-                getFromAPI(API_ROUTE.GET_DISCOUNTS, {
-                    callback: (json: Discount[]) => {
-                        commit('setDiscounts', { discounts: json, city })
-                    }, formatObject: { CITY: city }
-                });
+                getFromAPI<Discount>(API_ROUTE.GET_DISCOUNTS, {
+                    formatObject: { CITY: city }
+                }).then((discounts: Discount[]) => {
+                    commit('setDiscounts', { discounts, city })
+                })
             },
             async mapRecipeIdsToItemIds({ commit, getters }) {
                 commit('mapRecipeIdsToItemIds', getters.getRecipes)
@@ -100,7 +171,7 @@ export function createVueStore() {
         },
         getters: {
             getRecipes(state): Recipe[] {
-                return state.recipes.sort((a: Recipe, b: Recipe) => a.name.localeCompare(b.name)) ?? []
+                return state.recipes ?? []
             },
             getRecipesById: (_, getters): { [recipeId: string]: Recipe } => {
                 return getters.getRecipes.reduce((recipeMap: { [recipeId: string]: Recipe }, recipe: Recipe) => {
@@ -111,7 +182,7 @@ export function createVueStore() {
                 }, {})
             },
             getItems(state): Item[] {
-                return state.items.sort((a: Item, b: Item) => a.name.localeCompare(b.name)) ?? []
+                return state.items ?? []
             },
             getDiscounts: (state) => (city: string): Discount[] => {
                 return state.discounts[city] ?? []
