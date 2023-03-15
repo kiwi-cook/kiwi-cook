93862137223dbaa804e09cd0461fcccba018d3fb
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-03-16 00:00:17 +0100

    [frontend] Add component to display items

---
+++ frontend/src/components/item/SmallItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <ion-item lines="none">
+        <ion-avatar slot="start">
+            <img :src="item.imgUrl" />
+        </ion-avatar>
+        <ion-label color="light">
+            <h2>{{ item.name }}</h2>
+        </ion-label>
+    </ion-item>
+</template>
+
+<script lang="ts">
+import { Item } from '@/api/types';
+import { IonItem, IonAvatar, IonLabel } from '@ionic/vue';
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'SmallRecipe',
+  props: {
+    item: {
+      type: Object as PropType<Item>,
+      required: true,
+    },
+  },
+  components: {
+    IonItem, IonAvatar, IonLabel
+  }
+});
+</script>
