fb87d68e2053e104c2031a431869435f1fa007c7
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-03-20 02:28:00 +0100

    [frontend] Add reusable recipe component

---
+++ frontend/src/components/recipe/RecipeComponent.vue
@@ -0,0 +1,113 @@
+<template>
+    <ion-list lines="none">
+        <ion-item lines="none">
+            <RecipeHero :recipe="recipe"/>
+        </ion-item>
+        <ion-item lines="none">
+            <div class="center">
+                <div id="HeartSaveShareButton">
+                    <ion-button color="primary">
+                        <ion-icon slot="icon-only" :icon="heart"></ion-icon>
+                        {{ recipe?.likes }} Likes
+                    </ion-button>
+                    <ion-button color="primary">
+                        <ion-icon slot="icon-only" :icon="flagOutline">Save</ion-icon>
+                        Save
+                    </ion-button>
+                    <ion-button color="primary">
+                        <ion-icon slot="icon-only" :icon="shareOutline" aria-valuetext="Share Recipe"></ion-icon>
+                    </ion-button>
+                </div>
+            </div>
+        </ion-item>
+    </ion-list>
+
+    <ion-card v-if="ingredients?.length > 0">
+        <ion-card-header>
+            <ion-card-title color="light">
+                Ingredients
+            </ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+            <SmallItemContainer :items="ingredients"/>
+        </ion-card-content>
+    </ion-card>
+
+    <ion-card v-if="equipment?.length > 0">
+        <ion-card-header>
+            <ion-card-title color="light">
+                Equipment
+            </ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+            <SmallItemContainer :items="equipment"/>
+        </ion-card-content>
+    </ion-card>
+
+    <!-- <ion-card>
+        <ion-card-header>
+            <ion-card-title color="light">
+                Nutritional Values
+            </ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+            <ion-item>
+
+            </ion-item>
+        </ion-card-content>
+    </ion-card> -->
+
+    <template v-for="(step, stepIndex) in steps" :key="stepIndex">
+         <StepComponent :step="step" :stepIndex="stepIndex" />
+    </template>
+
+</template>
+
+<script lang="ts">
+import {computed, ComputedRef, defineComponent, PropType, toRefs} from 'vue';
+import {IonButton, IonIcon, IonItem, IonList} from '@ionic/vue';
+import {flagOutline, heart, shareOutline} from 'ionicons/icons';
+import {Item, Recipe, Step, voilaStep} from '@/api/types';
+import SmallItemContainer from '../item/SmallItemContainer.vue';
+import RecipeHero from "@/components/recipe/RecipeHero.vue";
+import StepComponent from "@/components/recipe/StepComponent.vue";
+
+export default defineComponent({
+    methods: {
+        voilaStep() {
+            return voilaStep
+        }
+    },
+    title: 'RecipeComponent',
+    props: {
+        recipe: {
+            type: Object as PropType<Recipe>,
+            required: true,
+        },
+    },
+    components: {
+        RecipeHero, SmallItemContainer, StepComponent,
+        IonIcon, IonButton, IonItem, IonList,
+    },
+    setup(props: any) {
+        const {recipe} = toRefs(props);
+
+        const itemsFromRecipe: ComputedRef<Item[]> = computed(() => recipe.value?.getItems());
+        const ingredients: ComputedRef<Item[]> = computed(() => itemsFromRecipe.value)
+        const equipment: ComputedRef<Item[]> = computed(() => []);
+        const steps: ComputedRef<Step[]> = computed(() => recipe.value?.steps)
+
+        return {
+            ingredients, equipment, steps,
+            heart, flagOutline, shareOutline
+        };
+    },
+});
+</script>
+
+<style scoped>
+#HeartSaveShareButton {
+    margin-top: 2%;
+}
+
+</style>
---
+++ frontend/src/components/recipe/StepComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <ion-card>
+        <ion-img :src="step?.imgUrl"/>
+        <ion-card-header>
+            <ion-card-title color="primary">
+                Step {{ stepIndex + 1 }}
+            </ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+            <SmallItemContainer :items="step?.items"/>
+            <ion-item lines="none">
+                <ion-text color="light">
+                    {{ step.description }}
+                </ion-text>
+            </ion-item>
+        </ion-card-content>
+    </ion-card>
+</template>
+
+<script lang="ts">
+import {IonCard, IonCardContent, IonCardHeader, IonImg, IonItem, IonText} from '@ionic/vue';
+import {defineComponent, PropType} from "vue";
+import {Step} from "@/api/types";
+import SmallItemContainer from "@/components/item/SmallItemContainer.vue";
+
+export default defineComponent({
+    name: 'RecipeStep',
+    props: {
+        stepIndex: {
+            type: Number,
+            required: true
+        },
+        step: {
+            type: Object as PropType<Step>,
+            required: true
+        }
+    },
+    components: {
+        SmallItemContainer,
+        IonCard, IonCardHeader, IonCardContent, IonItem, IonText, IonImg
+    },
+    setup() {
+        return {}
+    }
+})
+</script>
