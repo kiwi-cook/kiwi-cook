aa9b2b5cfbb17a899bb81ebdfe91322e4ba87f6f
am9zZWY <46693545+jpkmiller@users.noreply.github.com>
2023-05-05 00:13:57 +0200

    [frontend] Update iOS and Android and add async scrolling for Recipes

--- frontend/src/components/recipe/RecipeList.vue
+++ frontend/src/components/recipe/RecipeList.vue
@@ -1,14 +1,17 @@
 <template>
     <ion-list>
-        <template v-for="recipe in filteredRecipe" :key="recipe.name">
+        <template v-for="recipe in loadedRecipes" :key="recipe.name">
             <RecipePreview :recipe="recipe"/>
         </template>
     </ion-list>
+    <ion-infinite-scroll @ionInfinite="ionInfinite">
+        <ion-infinite-scroll-content></ion-infinite-scroll-content>
+    </ion-infinite-scroll>
 </template>
 
 <script lang="ts">
 import {IonList} from '@ionic/vue';
-import {computed, ComputedRef, defineComponent, ref, toRefs, watch} from 'vue';
+import {computed, ComputedRef, defineComponent, Ref, ref, toRefs, watch} from 'vue';
 import {arrowDown} from 'ionicons/icons';
 import {useTasteBuddyStore} from '@/storage';
 import {Recipe} from '@/api/types';
@@ -33,26 +36,43 @@ export default defineComponent({
 
         const store = useTasteBuddyStore();
         const recipes: ComputedRef<Recipe[]> = computed(() => store.getters.getRecipes);
-
-        const filteredRecipe = ref(recipes.value);
+        const loadedRecipes: Ref<Recipe[]> = ref([]);
+        const loadedRecipesIndex = ref(0);
 
         /**
          * Filter the ingredients
          */
         const handleFilter = () => {
             const query = filter.value?.toLowerCase() ?? '';
-            filteredRecipe.value = recipes.value.filter((recipe: Recipe) => JSON.stringify(recipe).toLowerCase().includes(query))
+            loadedRecipes.value = recipes.value.filter((recipe: Recipe) => {
+                return JSON.stringify(recipe)
+                    .toLowerCase()
+                    .includes(query)
+            })
         }
         watch(filter, () => {
             handleFilter();
         }, {immediate: true})
 
+        const loadNextRecipes = (amountLoaded = 15) => {
+            loadedRecipes.value.push(...recipes.value.slice(loadedRecipesIndex.value,
+                loadedRecipesIndex.value + amountLoaded))
+            loadedRecipesIndex.value += amountLoaded;
+        };
+
+        const ionInfinite = (ev: any) => {
+            loadNextRecipes();
+            setTimeout(() => ev.target.complete(), 500);
+        };
+
         watch(recipes, () => {
-            filteredRecipe.value = recipes.value;
+            loadedRecipesIndex.value = 0;
+            loadNextRecipes()
         })
 
         return {
-            filteredRecipe,
+            ionInfinite,
+            loadedRecipes,
             handleFilter,
             arrowDown
         };
