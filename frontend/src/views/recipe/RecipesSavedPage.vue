4456da97f7e1ac4fce0cedfe8ff519b9ae8b0d81
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2023-07-19 11:06:23 +0200

    [frontend] Add missing vue file

---
+++ frontend/src/views/recipe/RecipesSavedPage.vue
@@ -0,0 +1,59 @@
+<template>
+    <IonPage id="recipe-list-page">
+        <IonContent :fullscreen="true">
+            <div class="content">
+                <h1 class="header-title">
+                    Your favorite recipes
+                </h1>
+                <IonSearchbar v-model="filterInput" :debounce="500" placeholder="Search through your recipes"/>
+
+                <List :item-list="savedRecipes" :filter="filterInput"
+                      no-items-message="Save recipes to find them faster">
+                    <template #item="{ item }">
+                        <RecipePreview :recipe="item"/>
+                    </template>
+                </List>
+            </div>
+        </IonContent>
+    </IonPage>
+</template>
+
+<script lang="ts">
+import {computed, ComputedRef, defineComponent, ref} from 'vue';
+import {IonContent, IonPage, IonSearchbar, useIonRouter} from '@ionic/vue';
+import {addOutline, filter} from 'ionicons/icons';
+import {useTasteBuddyStore} from '@/storage';
+import {Recipe} from '@/tastebuddy/types';
+import List from "@/components/utility/List.vue";
+import RecipePreview from "@/components/recipe/RecipePreview.vue";
+
+export default defineComponent({
+    name: 'RecipeSavedPage',
+    components: {
+        RecipePreview, List, IonPage, IonSearchbar, IonContent
+    },
+    setup() {
+        const filterInput = ref('')
+
+
+        const router = useIonRouter()
+        const store = useTasteBuddyStore()
+        const savedRecipes: ComputedRef<Recipe[]> = computed(() => store.getSavedRecipes)
+
+        const isDevMode = computed(() => store.isDevMode)
+        const addRecipe = () => {
+            if (isDevMode.value) {
+                const newRecipeId = Recipe.newRecipe().update(store)._tmpId
+                router.push({name: 'RecipeEditor', params: {id: newRecipeId}})
+            }
+        }
+
+        return {
+            // recipe
+            savedRecipes, addOutline, addRecipe, isDevMode,
+            // filter
+            filterInput, filter
+        }
+    }
+});
+</script>
