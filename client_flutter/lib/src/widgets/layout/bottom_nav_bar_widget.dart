33a2f577a5425c07c7d57aab19d61b8655cf22f7
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-08-27 20:41:06 +0200

    Prepare navigation bar

---
+++ client_flutter/lib/src/widgets/layout/bottom_nav_bar_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    _selectedIndex = index;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.black,
+      unselectedItemColor: Colors.white54,
+      selectedItemColor: Colors.greenAccent,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'Favorites',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.explore),
+          label: 'Explore',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+      onTap: _onItemTapped,
+    );
+  }
+}
