6bc6378589c00381fc463f33cc9e8ecc048a5638
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-08-28 15:22:30 +0200

    Improve recipe provider

--- taste_buddy/lib/src/providers/recipe_provider.dart
+++ taste_buddy/lib/src/providers/recipe_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
@@ -13,6 +14,7 @@ class RecipeProvider with ChangeNotifier {
   List<Recipe> get recipes => _recipes;
   bool get isLoading => _isLoading;
 
+  /// Fetches recipes from the API. Caches the results unless `forceRefresh` is true.
   Future<List<Recipe>> fetchRecipes({bool forceRefresh = false}) async {
     if (_recipes.isNotEmpty && !forceRefresh) {
       return _recipes;
@@ -21,42 +23,71 @@ class RecipeProvider with ChangeNotifier {
     _isLoading = true;
     notifyListeners();
 
-    final response = await http.get(Uri.parse(
-      kDebugMode
-          ? 'http://127.0.0.1:8000/recipe/'
-          : 'https://taste-buddy.onrender.com/recipe/',
-    ));
+    try {
+      final response = await http.get(Uri.parse(
+        kDebugMode
+            ? 'http://127.0.0.1:8000/recipe/'
+            : 'https://taste-buddy.onrender.com/recipe/',
+      ));
 
-    if (response.statusCode == 200) {
-      // Ensure response body is in UTF-8
-      final decodedResponse = utf8.decode(response.bodyBytes);
-      final jsonList = jsonDecode(decodedResponse)['response'];
+      if (response.statusCode == 200) {
+        // Ensure response body is in UTF-8
+        final decodedResponse = utf8.decode(response.bodyBytes);
+        final Map<String, dynamic> jsonResponse = jsonDecode(decodedResponse);
 
-      _recipes =
-          jsonList.map<Recipe>((recipe) => Recipe.fromJson(recipe)).toList();
-    } else {
-      throw Exception('Failed to load recipes');
+        if (jsonResponse.containsKey('response')) {
+          final jsonList = jsonResponse['response'] as List<dynamic>;
+
+          // Convert the JSON response into a list of Recipe objects
+          _recipes = jsonList
+              .map<Recipe>((recipe) => Recipe.fromJson(recipe))
+              .toList();
+        } else {
+          throw const FormatException('Unexpected response format');
+        }
+      } else {
+        throw HttpException(
+            'Failed to load recipes: ${response.statusCode}');
+      }
+    } on FormatException catch (e) {
+      // Handle JSON decoding error
+      throw Exception('Failed to decode recipes: $e');
+    } on http.ClientException catch (e) {
+      // Handle client-side errors
+      throw Exception('Client error occurred: $e');
+    } catch (e) {
+      // Handle any other type of exception
+      throw Exception('An error occurred: $e');
+    } finally {
+      _isLoading = false;
+      notifyListeners();
     }
 
-    _isLoading = false;
-    notifyListeners();
     return _recipes;
   }
 
+  /// Finds a recipe by its ID. Returns null if not found.
   Recipe? findById(String id) {
     return _recipes.firstWhereOrNull((recipe) => recipe.id == id);
   }
 
+  /// Returns the recipe of the day based on the day of the year.
   Recipe? recipeOfTheDay() {
-    // If there are no recipes, return null
     if (_recipes.isEmpty) {
       return null;
     }
 
     DateTime now = DateTime.now();
     int dayOfYear = now.difference(DateTime(now.year, 1, 1)).inDays;
-    int seed = dayOfYear;
-    int randomIndex = (seed * 16807) % recipes.length;
-    return recipes[randomIndex];
+    int randomIndex = Random(dayOfYear).nextInt(_recipes.length);
+    return _recipes[randomIndex];
   }
+}
+
+class HttpException implements Exception {
+  final String message;
+  HttpException(this.message);
+
+  @override
+  String toString() => 'HttpException: $message';
 }
