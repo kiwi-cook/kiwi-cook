1df296959100049ad66117df9f8dd61577bfd622
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-09-08 18:38:40 +0200

    Add more origins

--- taste_buddy_server/server/app.py
+++ taste_buddy_server/server/app.py
@@ -20,13 +20,15 @@ origins = [
     "http://localhost:8080",
     "http://localhost:8100",
     "https://taste-buddy.github.io",
+    "https://taste-buddy.uk",
+    "https://api.taste-buddy.uk",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "HEAD"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 app.include_router(recipe.router)
