0931bc0df4e97ff5bb24be9d7d7d8dc5cb130a3f
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-09-18 11:02:07 +0200

    Improve token handling

--- taste_buddy/lib/src/providers/user_provider.dart
+++ taste_buddy/lib/src/providers/user_provider.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
 import 'package:taste_buddy/src/utils/fetch.dart';
 import '../models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider with ChangeNotifier {
   User _user = User();
-  String _accessToken = '';
   User get user => _user;
   String get accessToken => _accessToken;
   bool get isLoggedIn => _accessToken.isNotEmpty;
+
+  String _accessToken = '';
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
 
   // Constructor
   UserProvider() {
@@ -26,35 +28,76 @@ class UserProvider with ChangeNotifier {
   }
 
   Future<void> authenticate(String username, String password) async {
-    final response = await http.post(
-      Uri.parse(
-        kDebugMode
-            ? 'http://127.0.0.1:8000/users/token'
-            : 'https://taste-buddy.uk/users/token',
-      ),
-      body: {
-        'username': username,
-        'password': password,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
-      _accessToken = jsonResponse['access_token'];
+    try {
+      final token = await sendRequest('/users/token',
+          method: 'POST',
+          body: {
+            'username': username,
+            'password': password,
+          },
+          isFormData: true) as Map<String, dynamic>;
+      final accessToken = token['access_token'] as String;
+      saveToken(accessToken);
       await fetchUser();
       notifyListeners();
-    } else {
-      throw Exception('Failed to authenticate: ${response.statusCode}');
+    } catch (e) {
+      throw Exception('Failed to authenticate: $e');
     }
   }
 
   Future<void> loadToken() async {
-    const storage = FlutterSecureStorage();
-    final token = await storage.read(key: 'jwt');
-    if (token != null) {
-      _accessToken = token;
-      notifyListeners();
+    try {
+      String? loadedToken;
+      if (!kIsWeb) {
+        // Use flutter_secure_storage for mobile platforms
+        loadedToken = await _secureStorage.read(key: "jwt");
+      } else {
+        // Use shared_preferences for web platform
+        final prefs = await SharedPreferences.getInstance();
+        loadedToken = prefs.getString('jwt');
+      }
+
+      if (loadedToken != _accessToken) {
+        _accessToken = loadedToken ?? '';
+        notifyListeners();
+      }
+    } catch (e) {
+      print('Error loading token: $e');
+      // Fallback to SharedPreferences if FlutterSecureStorage fails
+      if (!kIsWeb) {
+        final prefs = await SharedPreferences.getInstance();
+        _accessToken = prefs.getString('jwt') ?? '';
+        notifyListeners();
+      }
     }
+  }
+
+  Future<void> saveToken(String? token) async {
+    if (token == null) {
+      return;
+    }
+
+    _accessToken = token;
+
+    try {
+      if (!kIsWeb) {
+        // Use flutter_secure_storage for mobile platforms
+        await _secureStorage.write(key: "jwt", value: token);
+      } else {
+        // Use shared_preferences for web platform
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('jwt', token);
+      }
+    } catch (e) {
+      print('Error saving token: $e');
+      // Fallback to SharedPreferences if FlutterSecureStorage fails
+      if (!kIsWeb) {
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('jwt', token);
+      }
+    }
+
+    notifyListeners();
   }
 
   Future<void> fetchUser() async {
@@ -62,8 +105,8 @@ class UserProvider with ChangeNotifier {
       return; // Don't fetch if there's no token
     }
     try {
-      final jsonResponse =
-          await sendRequest('/users/me', method: 'GET', body: {}, accessToken: _accessToken);
+      final jsonResponse = await sendRequest('/users/me',
+          method: 'GET', body: {}, accessToken: _accessToken);
       _user = User.fromJson(jsonResponse);
       print('User: ${_user.username}');
       notifyListeners();
@@ -86,12 +129,18 @@ class UserProvider with ChangeNotifier {
         print('Removing recipe $recipeId');
         _user.recipes.remove(recipeId);
         await sendRequest('/users/me/recipes/remove/',
-            method: 'POST', body: {'recipe_id': recipeId}, isFormData: true, accessToken: _accessToken);
+            method: 'POST',
+            body: {'recipe_id': recipeId},
+            isFormData: true,
+            accessToken: _accessToken);
       } else {
         print('Adding recipe $recipeId');
         _user.recipes.add(recipeId);
         await sendRequest('/users/me/recipes/add/',
-            method: 'POST', body: {'recipe_id': recipeId}, isFormData: true, accessToken: _accessToken);
+            method: 'POST',
+            body: {'recipe_id': recipeId},
+            isFormData: true,
+            accessToken: _accessToken);
       }
       notifyListeners();
     } catch (e) {
