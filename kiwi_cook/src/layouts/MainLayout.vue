4b56ebfac9d756317f6717cc0895808d616fabc0
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-10-03 14:31:49 +0200

    Move the KiwiCook logo to the layout

--- kiwi_cook/src/layouts/MainLayout.vue
+++ kiwi_cook/src/layouts/MainLayout.vue
@@ -1,5 +1,9 @@
 <template>
   <q-layout view="hHh lpR fFf">
+    <div class="q-pa-md bg-black text-white" @click="$router.push('/')">
+      <div class="text-h4 text-weight-bold">KiwiCook 🥝</div>
+      <div class="text-subtitle1 text-green-14">Reclaim your kitchen</div>
+    </div>
 
     <q-page-container>
       <router-view/>
