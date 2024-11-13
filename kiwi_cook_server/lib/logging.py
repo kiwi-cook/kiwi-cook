56dba39956f283b37bad1f38585a5f60fb12ba41
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2024-11-14 00:38:55 +0100

    Add missing logging file

---
+++ kiwi_cook_server/lib/logging.py
@@ -0,0 +1,27 @@
+import logging
+from logging.config import dictConfig
+
+# Configure logging
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(message)s",
+            "use_colors": None,
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    },
+    "loggers": {
+        "kiwicook": {"handlers": ["default"], "level": "INFO"},
+    },
+}
+dictConfig(logging_config)
+logger = logging.getLogger("kiwicook")
